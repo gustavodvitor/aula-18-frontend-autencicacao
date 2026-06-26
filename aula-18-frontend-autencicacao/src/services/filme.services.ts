1fd2e8ca53f14f79c1f5ec21a2731a0cd41f4c80
Gustavo Daniel Vitor <d2021020134@unifei.edu.br>
2026-06-26 20:10:14 -0300

    feat(service): adiciona updateFilme e deleteFilme

---
+++ aula-18-frontend-autencicacao/src/services/filme.services.ts
@@ -0,0 +1,72 @@
+import {Filme, CreateFilmeDTO, UpdateFilmeDTO} from "@/tipos/filme";
+
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL;
+
+export async function getFilmes(cookieHeader?: string): Promise<Filme[]>
+{
+    const response = await fetch(`${API_URL}/filmes`,{
+        headers: {
+            Cookie: cookieHeader ?? "",
+        }
+    });
+    
+    if (!response.ok) {
+        throw new Error("Não autenticado");
+    }
+
+    const dados = await response.json();
+
+    return dados;
+
+}
+
+export async function getFilme(id: string, cookieHeader?: string): Promise<Filme> {
+  
+ 
+    const response = await fetch(`${API_URL}/filmes/${id}`,{
+        headers: {
+            Cookie: cookieHeader ?? ""
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error("Não autenticado");
+    }
+
+    return response.json();
+}
+
+export async function createFilmes(filme: CreateFilmeDTO): Promise<void>
+{
+    const response = await fetch(`${API_URL}/filmes`,{
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(filme),
+    });
+
+    if (!response.ok) {
+        throw new Error("Erro");
+    }
+}
+
+export async function updateFilme(id: number, dados: Partial<Filme>) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/filmes/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify(dados),
+  });
+  return res.json();
+}
+
+export async function deleteFilme(id: number) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/filmes/${id}`, {
+    method: "DELETE",
+    credentials: "include",
+  });
+  return res;
+}
